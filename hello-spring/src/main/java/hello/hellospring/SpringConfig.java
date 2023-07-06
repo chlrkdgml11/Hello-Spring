a10e8d9b11f02ef0bd6520c39d849be08f8956f4
chlrkdgml11 <chlrkdgml11@naver.com>
2023-07-07 01:30:11 +0900

    add comments

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,19 +13,79 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-/*    private DataSource dataSource;
+    /*    MemoryMemberRepository
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }*/
+
+
+
+    /*    JdbcMemberRepository
+    private final DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcTemplateMemberRepository(dataSource);
     }*/
 
-/*    private EntityManager em;
 
+
+    /*    JdbcTemplateMemberRepository
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcTemplateMemberRepository(dataSource);
+    }*/
+
+
+
+    /*    JpaMemberRepository
+    private final EntityManager em;
+
+    @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
     }*/
 
+
+    //Spring Data JPA
     private final MemberRepository memberRepository;
 
     @Autowired
@@ -36,13 +97,4 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
-
-/*    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-
-    }*/
 }
